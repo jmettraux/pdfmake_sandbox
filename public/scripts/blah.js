4130603dc60796b030c9af7a3acccd0698bc3b30
John Mettraux <jmettraux@gmail.com>
2017-06-14 16:34:47 +0900

    Use table in footer

--- public/scripts/blah.js
+++ public/scripts/blah.js
@@ -1,4 +1,29 @@
 
+// Styles, according to
+// https://stackoverflow.com/questions/32291112/pdfmake-api-is-there-a-list-of-styles-fonts-capabilities
+//
+// ['font',
+// 'fontSize',
+// 'bold',
+// 'italics',
+// 'alignment',
+// 'color',
+// 'columnGap',
+// 'fillColor',
+// 'decoration',
+// 'decorationStyle',
+// 'decorationColor',
+// 'background',
+// 'lineHeight'
+// //'tableCellPadding'
+// // 'cellBorder',
+// // 'headerCellBorder',
+// // 'oddRowCellBorder',
+// // 'evenRowCellBorder',
+// // 'tableBorder'
+// 'marginLeft' // self
+// ]
+
 var Blah = (function() {
 
   "use strict";
@@ -27,16 +52,36 @@ var Blah = (function() {
 
     // styles
 
-    def.styles.italian = { italic: true, fontSize: 7 };
-    def.styles.motto = { italic: true, fontSize: 7 };
-    def.styles.pagination = { fontSize: 7, marginLeft: 10 };
+    def.styles.italian = {
+      italic: true, fontSize: 7 };
+    def.styles.banner = {
+      italic: true, fontSize: 10, marginLeft: 14, fillColor: 'blue',
+      color: 'white', width: 300 };
+    def.styles.pagination = {
+      fontSize: 7, marginLeft: 14 };
 
     // header and footer
 
     //def.header = 'nada';
+
     //def.footer = [ { text: 'Glow the Value!' } ];
     def.footer = function(p, tp) {
-      return { text: '' + p + '/' + tp, style: 'pagination' };
+      return [
+        //{ text: 'Glow the Value', style: 'motto' },
+        //{ columns: [
+        //  { width: '90%', text: 'a', marginLeft: 14, style: 'motto' },
+        //  { width: '*', text: 'b', style: 'motto' },
+        //] },
+        //'Glow the Value', style: 'motto' },
+        { table: {
+          widths: [ '80%', '*' ],
+          body: [ [
+            { text: 'Glow the Value', style: 'banner' },
+            { text: 'Logo', style: 'banner' }
+          ] ]
+        }, layout: { defaultBorder: false }, margin: [ 13, 0 ] },
+        { text: '' + p + '/' + tp, style: 'pagination' }
+      ];
     };
 
     // content
